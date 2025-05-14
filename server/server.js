1d499d6463a5c9e73a51f515c136652fd6de7356
Ruthuran Muralirajan <ruthuranruthuran2@gmail.com>
2025-05-14 16:41:47 +0530

    Update server.js

--- server/server.js
+++ server/server.js
@@ -6,8 +6,7 @@ import cors from 'cors';
 import authRoutes from './routes/authRoutes.js';
 import adminRoutes from './routes/adminRoutes.js';
 import courseRoutes from './routes/courseRoutes.js';
-import studentRoutes from './routes/studentRoutes.js';
-import enrollmentRoutes from './routes/enrollments.js';
+llments.js';
 import assignmentRoutes from './routes/assignments.js';
 import connectDB from './db.js';
 
