5d2d7b75700a559e061f13745c63c4534644723b
Ruthuran Muralirajan <ruthuranruthuran2@gmail.com>
2025-05-14 15:39:07 +0530

    Update server.js

--- server/server.js
+++ server/server.js
@@ -51,5 +51,5 @@ app.use((err, req, res, next) => {
 // Start Server
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
-  console.log(Server is running on port ${PORT});
+  console.log(` Server is running on port ${PORT}`);
 });
