6ac5c279f98beda56d9b5e64805ed1639bc7bf20
Ruthuran Muralirajan <ruthuranruthuran2@gmail.com>
2025-05-15 15:33:53 +0530

    Update AdminMentors.jsx

--- client/src/components/admin/AdminMentors.jsx
+++ client/src/components/admin/AdminMentors.jsx
@@ -31,7 +31,7 @@ const AddMentorForm = () => {
   
     const fetchCourses = async () => {
       try {
-        const res = await axios.get(`${import.meta.env.VITE_API_URL}/api/courses`);
+        const res = await axios.get(COURSES_API_URL);
         setCourses(res.data);
       } catch (err) {
         console.error("Error fetching courses:", err);
